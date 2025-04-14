dc0bb1633d7d2ae293e9f90da85c1e1ec56a8f9a
DarrenBoyo <boyodarren66@gmail.com>
2025-04-13 20:30:20 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+#include <stdexcept> // For std::invalid_argument
+
+using namespace std;
+
+int hex2Dec(const string& hex);
+int hexCharToDecimal(char ch);
+
+int main()
+{
+    cout << "Enter a hex number: ";
+    string hex;
+    cin >> hex;
+
+    try {
+        cout << "The decimal value for hex number " << hex
+             << " is " << hex2Dec(hex) << endl;
+    } catch (const invalid_argument& e) {
+        cerr << "Error: " << e.what() << endl;
+    }
+
+    return 0;
+}
+
+int hex2Dec(const string& hex)
+{
+    int decimalValue = 0;
+    for (unsigned i = 0; i < hex.size(); i++) {
+        decimalValue = decimalValue * 16 + hexCharToDecimal(hex[i]);
+    }
+    return decimalValue;
+}
+
+int hexCharToDecimal(char ch)
+{
+    ch = toupper(ch);
+
+    if (ch >= '0' && ch <= '9') {
+        return ch - '0';
+    } else if (ch >= 'A' && ch <= 'F') {
+        return 10 + ch - 'A';
+    } else {
+        throw invalid_argument(string("Invalid hex character: ") + ch);
+    }
+}
